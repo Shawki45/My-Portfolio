249a5e698acd554120a0de223a3c9427a240fc1d
Shawki45 <shawkihaifa@yahoo.com>
2024-03-03 13:55:37 -0500

    added 2 more the projects tab

--- client/src/components/pages/Blog.jsx
+++ client/src/components/pages/Blog.jsx
@@ -4,6 +4,13 @@ export default function Blog() {
       <h1>Project Page</h1>
       <a href='https://github.com/m-beke/StreamScape.git'>StreamScape Project</a>
       <img src="images/noodle.png" ></img>
+      <a href='https://github.com/Shawki45/Flavor-Trail.git'>Flavor Trail Project</a>
+      <img src="images/flavor-trail-screenshot.png"></img>
+      <a href='https://github.com/Shawki45/Code-Refractor.git'>Code Refractor Challenge</a>
+      <img src='images/Screenshot-website.png'></img>
+
+
+
       <p>
         
       </p>
