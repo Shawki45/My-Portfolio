f79377f1d222d62b47188c09074707d3247e1fd4
Shawki45 <shawkihaifa@yahoo.com>
2024-03-03 14:20:48 -0500

    added 2 more challenges to the project tab

--- client/src/components/pages/Blog.jsx
+++ client/src/components/pages/Blog.jsx
@@ -2,13 +2,27 @@ export default function Blog() {
   return (
     <div>
       <h1>Project Page</h1>
+      <section>
       <a href='https://github.com/m-beke/StreamScape.git'>StreamScape Project</a>
-      <img src="images/noodle.png" ></img>
+      <img src="images/Streamscape.png" ></img>
+      </section>
+      <section>
       <a href='https://github.com/Shawki45/Flavor-Trail.git'>Flavor Trail Project</a>
-      <img src="images/flavor-trail-screenshot.png"></img>
+      <img src="images/flavor-trail-screenshot.png" class= "noodle"></img>
+      </section>
+      <section>
       <a href='https://github.com/Shawki45/Code-Refractor.git'>Code Refractor Challenge</a>
       <img src='images/Screenshot-website.png'></img>
-
+      </section>
+      <section>
+        <a href='https://github.com/Shawki45/Food-Quiz.git'>Food Quiz Challenge</a>
+        <img src='images/foodquizstart.png'></img>
+        </section>
+        <section>
+          <a href='https://github.com/Shawki45/Taking-Notes.git'>Taking Notes Challenge</a>
+          <img src='images/notetaker.png'></img>
+          </section>
+        
 
 
       <p>
