48bc697b225d22685b3895efee9c3f0166b57763
Shawki45 <shawkihaifa@yahoo.com>
2024-03-03 14:38:29 -0500

    did the about me page

--- client/src/components/pages/About.jsx
+++ client/src/components/pages/About.jsx
@@ -3,13 +3,12 @@ export default function About() {
     <div>
       <h1>About Page</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+       Hello there and welcome. My name is Shawki and I am a web delevoper or in progress web delevoper.
+       A little about me is i am 27 years old and i chose web delevopment to further my skills in life and hopefully get a job as well.
+       My hobbies inlcude bowling and go-karting although it has been a few months since i have done that, but i do enjoy them. 
+       I am currently employed but i would to add more things that i have done to my resume. 
       </p>
+      <img src='images/me.jpg'></img>
     </div>
   );
 }
