9762c81ea7d8fda41a7bf96bcf738ee1ae55e06f
Shawki45 <shawkihaifa@yahoo.com>
2024-03-05 15:43:40 -0500

    added to the homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,9 @@ export default function Home() {
       <h1>Home Page</h1>
       <p>
         Welcome to the portifilio of Shawki. if you have any questions go to my contact page to ask me questions.
+      
       </p>
+      <img src='images/homepage.png'></img>
     </div>
   );
 }
