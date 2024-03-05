b3b45c30e4d0efb40b1e823996c26046147aff80
Shawki45 <shawkihaifa@yahoo.com>
2024-03-04 21:42:24 -0500

    added css

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -3,28 +3,29 @@ export default function Blog() {
     <div>
       <h1>Project Page</h1>
       <section>
+      <div class= "img1"></div>
       <a href='https://github.com/m-beke/StreamScape.git'>StreamScape Project</a>
-      <img src="images/Streamscape.png" ></img>
+      <img src="images/Streamscape.png"></img>
       </section>
       <section>
       <a href='https://github.com/Shawki45/Flavor-Trail.git'>Flavor Trail Project</a>
-      <img src="images/flavor-trail-screenshot.png" class= "noodle"></img>
+      <img src="images/flavor-trail-screenshot.png" class= "img2"></img>
       </section>
       <section>
       <a href='https://github.com/Shawki45/Code-Refractor.git'>Code Refractor Challenge</a>
-      <img src='images/Screenshot-website.png'></img>
+      <img src='images/Screenshot-website.png' class= "img3"></img>
       </section>
       <section>
         <a href='https://github.com/Shawki45/Food-Quiz.git'>Food Quiz Challenge</a>
-        <img src='images/foodquizstart.png'></img>
+        <img src='images/foodquizstart.png' class= "img4"></img>
         </section>
         <section>
           <a href='https://github.com/Shawki45/Taking-Notes.git'>Taking Notes Challenge</a>
-          <img src='images/notetaker.png'></img>
+          <img src='images/notetaker.png' class= "img5"></img>
           </section>
           <section>
             <a href='https://github.com/Shawki45/Text-Editor.git'></a>
-            <img src='images/texteditor.png'></img>
+            <img src='images/texteditor.png' class= "img6"></img>
             </section>
         
 
